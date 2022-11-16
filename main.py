b600cfad2e30ad7f777e4eb28274ecd4b9b6d62a
Diego <policarpo16@yahoo.com.br>
2022-11-16 11:28:47 -0300

    Version 3.0

--- main.py
+++ main.py
@@ -3,29 +3,64 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+# função para remover o modo grafico do Selenium
+from selenium.webdriver.chrome.options import Options
 import time
+from datetime import datetime
+import pandas as pd
 
-# Criando o executavel para emular o navegador sem precisar usar o arquivo do Webdriver
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-# Direciona para abrir o site 
-driver.get("https://economia.uol.com.br/cotacoes/bolsas/")
+def coleta_dados():
+    options = Options()
+    options.add_argument('--headless')
 
-lista_acoes = ['B3SA3', 'KLBN4', 'ABEV3', 'BBAS3', 'EGIE3', 'ITSA4', 'PETR3', 'PETR4', 'VALE3', 'TAEE11', 'WEGE3', 'EMBR3']
+    # Criando o executavel para emular o navegador sem precisar usar o arquivo do Webdriver
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=options
+    )
 
-for acao in lista_acoes:
-    input_busca = driver.find_element(By.ID, 'filled-normal')
-    input_busca.send_keys(acao)
-    time.sleep(2)
-    input_busca.send_keys(Keys.ENTER)
-    time.sleep(2)
+    # Direciona para abrir o site 
+    driver.get("https://economia.uol.com.br/cotacoes/bolsas/")
 
-    span_valor = driver.find_element(By.XPATH, '//span[@class="chart-info-val ng-binding"]')
-    valor = span_valor.text
-    print(f'Valor da contação da {acao} é atualmente R${valor}')
+    lista_acoes = ['B3SA3', 'KLBN4', 'ABEV3', 'BBAS3', 'EGIE3', 'ITSA4', 'PETR3', 'PETR4', 'VALE3', 'TAEE11', 'WEGE3', 'EMBR3']
+    lista_valor = list()
+    lista_dt = list()
+
+    for acao in lista_acoes:
+        input_busca = driver.find_element(By.ID, 'filled-normal')
+        input_busca.send_keys(acao)
+        time.sleep(2)
+        input_busca.send_keys(Keys.ENTER)
+        time.sleep(2)
+        span_valor = driver.find_element(By.XPATH, '//span[@class="chart-info-val ng-binding"]')
+        valor = span_valor.text
+        
+        lista_valor.append(valor)
+        lista_dt.append(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+        
+        print(f'Empresa: {acao} cotação atual R${valor}.')
+        
+    dados = {
+        'empresas': lista_acoes,
+        'valor': lista_valor,
+        'data_hora': lista_dt,
+    }
+    
+    return dados
+
+def gera_excel(dados, filename):
+    df_acoes = pd.DataFrame(dados)
+    # Instalar o pacote openpyxl para manipulação de xls e xlsx
+    # para remover o indice, informe False no index
+    df_acoes.to_excel(filename, index=False)
+
 
 # Para o navegador não fechar
-input('')
+# input('')
+
+dados = coleta_dados()
+gera_excel(dados, './empresas_acoes.xlsx')
 
 
  
